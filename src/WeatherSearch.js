7f73e551129154895ff579b2886380a6f5859e03
Meklit Habtamu <124443510+meklithab@users.noreply.github.com>
2024-07-08 23:32:47 +0300

    Added Unit Conversion And Default Value

--- src/WeatherSearch.js
+++ src/WeatherSearch.js
@@ -1,9 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
+import ReactAnimatedWeather from 'react-animated-weather';
 
+import "./Weather.css"
 export default function WeatherSearch() {
-    const [city, setCity] = useState("");
+    const [city, setCity] = useState();
     const [weather, setWeather] = useState({
+        city: null,
         temp: null,
         desc: null,
         hum: null,
@@ -12,8 +15,62 @@ export default function WeatherSearch() {
 
     });
 
+
+
+    const [weatherIcon, setWeatherIcon] = useState("RAIN")
+
+
+    const date = new Date()
+    const day = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    const time = date.toLocaleTimeString();
+
+    const iconCode = {
+        "01d": "CLEAR_DAY",
+        "01n": "CLEAR_NIGHT",
+        "02d": "PARTLY_CLOUDY_DAY",
+        "02n": "PARTLY_CLOUDY_NIGHT",
+        "03d": "CLOUDY",
+        "03n": "CLOUDY",
+        "04d": "CLOUDY",
+        "04n": "CLOUDY",
+        "09d": "RAIN",
+        "09n": "RAIN",
+        "10d": "RAIN",
+        "10n": "RAIN",
+        "11d": "RAIN",
+        "11n": "RAIN",
+        "13d": "SNOW",
+        "13n": "SNOW",
+        "50d": "FOG",
+        "50n": "FOG",
+        "13d": "SLEET",
+        "13n": "SLEET",
+        "50d": "WIND",
+        "50n": "WIND"
+
+    };
+
+
     const apiKey = "8ca7dd4e61360b90fb66918853670e48";
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    useEffect(() => {
+        axios.get(`https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`).then((response) => {
+            console.log(response.data);
+            setWeather({
+                city: "London",
+                temp: Math.round(response.data.main.temp),
+                desc: response.data.weather[0].description,
+                hum: response.data.main.humidity,
+                wind: response.data.wind.speed,
+                icon: response.data.weather[0].icon,
+            });
+
+            setWeatherIcon(iconCode[response.data.weather[0].icon]);
+        });
+    }, [city == ""])
+
+
 
     const handleInputChange = (e) => {
         setCity(e.target.value);
@@ -25,44 +82,89 @@ export default function WeatherSearch() {
             axios.get(apiUrl).then((response) => {
                 console.log(response.data);
                 setWeather({
-                    temp: response.data.main.temp,
+                    city: city,
+                    temp: Math.round(response.data.main.temp),
                     desc: response.data.weather[0].description,
                     hum: response.data.main.humidity,
                     wind: response.data.wind.speed,
                     icon: response.data.weather[0].icon,
                 });
+
+                setWeatherIcon(iconCode[response.data.weather[0].icon]);
             });
         } catch (error) {
+
             alert("Unable to fetch data.");
         }
     };
 
-    function displayData() {
-        console.log(weather);
-        return (
-            <ul>
-                <li>Temperature: {weather.temp}</li>
-                <li>Description: {weather.desc}</li>
-                <li>Humidity: {weather.hum}</li>
-                <li>Wind: {weather.wind}</li>
-                <li>
-                    <img src={`http://openweathermap.org/img/w/${weather.icon}.png`} alt="Weather Icon" />
-                </li>
-            </ul>
-        );
-    }
+
+    const toCelsius = (e) => {
+        e.preventDefault();
+        if (weather.temp) {
+            axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+            ).then((response) => {
+
+                const celsiusValue = Math.round(response.data.main.temp)
+                setWeather({ ...weather, temp: celsiusValue });
+
+
+            });
+
+
+        }
+    };
+
+    const toFahrenheit = (e) => {
+        e.preventDefault();
+        if (weather.temp) {
+            const fahrenheitValue = (Math.round(weather.temp * 9 / 5)) + 32;
+            setWeather({ ...weather, temp: fahrenheitValue });
+
+        }
+    };
+
 
     return (
         <>
-            <form onSubmit={getCity}>
-                <input
-                    type="text"
-                    onChange={handleInputChange}
-                    placeholder="Enter a city..."
-                />
-                <input type="submit" value="Search" />
-            </form>
-            {weather.temp !== null && displayData()}
+            <div className="container">
+
+                <form onSubmit={getCity}>
+                    <input
+                        className="search"
+                        type="text"
+                        onChange={handleInputChange}
+                        placeholder="Enter a city..."
+                    />
+                    <input className="button" type="submit" value="Search" />
+                </form>
+                <div className="section-1">
+
+                    <h4>{weather.city}</h4>
+                    <p>{day[date.getDay() - 1]} {time}</p>
+                    <p>{weather.desc}</p>
+
+                </div>
+
+                <div className="cont">
+                    <div className="box1">
+                        <h2>
+                            <ReactAnimatedWeather
+                                icon={weatherIcon}
+                                size={64}
+
+                            />
+                            <span>{weather.temp}</span><sup><a href="" onClick={toCelsius}>C</a>|<a href="" onClick={toFahrenheit}>F</a></sup>
+                        </h2>
+                    </div>
+                    <div className="box2">
+                        <p>Humidity:{weather.hum}</p>
+                        <p>Wind:{weather.wind}</p>
+
+                    </div>
+                </div>
+
+            </div>
         </>
     );
 }
